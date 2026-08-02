91fca39581f00b71d22bc3b3b66a2296a5a1fd3b
vispan77 <vishalpandey2332@gmail.com>
2026-08-02 12:53:20 +0530

    updated backedn url in the frontend

--- client/utils/api.js
+++ client/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_LOCAL_SERVER,
+    baseURL: import.meta.env.VITE_GATEWAY_URL,
     withCredentials: true
 
 })
